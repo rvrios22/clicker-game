7efbc614abb7a814c064aad6e4ea6f66398fc158
Rafael Rios <rvrios22@gmail.com>
2024-02-21 15:46:22 -0800

    add: interaction for Welcome page. TODO: animation for Welcome component

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./welcome.css";
 
 function Welcome() {
+    const [isButtonClicked, setButtonIsClicked] = useState(false)
+
+    const handleButtonClick = () => {
+        setButtonIsClicked(true)
+    }
   return (
-    <div className="welcome-container">
-        <h1 className="welcome-header">Welcome to the Clicker!</h1>
-        <p>I'm glad you're here. The clicker is a clicker style game where the point is to... CLICK! Easy enough I know. Along your clicking journey you'll acquire various power ups and skils that will help you click even faster! Grab your mouse and get ready to click!</p>
+    <div className={!isButtonClicked ? 'welcome-container' : 'welcome-container welcome-animation'}>
+        <h1 className="welcome-header luckiest-guy">Welcome to the Clicker!</h1>
+        <p className=" welcome-text protest-revolution">I'm glad you're here. The clicker is a clicker style game where the point is to... CLICK! Easy enough I know. Along your clicking journey you'll acquire various power ups and skils that will help you click even faster! Grab your mouse and get ready to click!</p>
+        <button onClick={handleButtonClick} className="welcome-button">Click Here to Get Started!</button>
     </div>)
   ;
 }
